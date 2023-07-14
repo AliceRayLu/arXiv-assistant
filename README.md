72e428c6c816006c13cf627132cba53f69b16691
A.R.L <luminyouyazj@126.com>
2023-07-14 20:12:35 +0800

    initialize project

---
+++ README.md
@@ -0,0 +1,9 @@
+# arXiv-assistant
+use LangChain to build LLM-based arXiv, easier search, easier use
+
+## How to Use
+Run the following command:
+`npm run serve`
+to start the project locally
+> attention: the project is based on *Vicuna*, which can only be visited
+> via NJU campus network
---
+++ src/pages/homePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <el-input placeholder="Type in whole sentences to search for papers." v-model="userInput">
+      <template #append>
+        <el-button type="primary" :icon="Search">search</el-button>
+      </template>
+    </el-input>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import {Search} from '@element-plus/icons-vue'
+const userInput = ref('')
+</script>
+
+<style scoped>
+
+</style>
